c1ac13c217414d4f0a501e74f624fd055fa60e2c
daniel sarabia viera <dani_sv_ctv@hotmail.com>
2017-09-03 10:19:53 +0200

    Uploading website

---
+++ MvcDemo/ViewModels/ContactViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcDemo.ViewModels
+{
+    public class ContactViewModel
+    {
+		[Required(ErrorMessage = "Please enter your title")]
+		public string Title { get; set; }
+
+		[Required(ErrorMessage = "Please enter your name")]
+		public string FullName { get; set; }
+
+		public string Company { get; set; }
+
+		[Required(ErrorMessage = "Please enter a contact email")]
+		[RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Please enter a contact phone")]
+		public string Phone { get; set; }
+
+		[Required(ErrorMessage = "The message field can not be empty")]
+		public string Message { get; set; }
+	}
+}
---
+++ MvcDemo/wwwroot/js/site.js
@@ -0,0 +1,100 @@
+﻿$(function () {
+	// Initializations
+	loadStackOverflowAnswers();
+	loadStackOverflowUsers();
+
+	// Private Methods
+	function loadStackOverflowAnswers() {
+		$.ajax({
+			url: "https://api.stackexchange.com/2.2/questions?page=1&pagesize=5&order=desc&sort=hot&site=stackoverflow",
+			type: "GET",
+			dataType: "json",
+			cache: false,
+			success: function (data) {
+				console.log(data);
+				var table = $('<table></table>');
+				table.addClass('table table-striped');
+				var header = $('<thead></thead>');
+				var headerRow = $('<tr></tr>');
+				headerRow.append('<th>Title</th>');
+				headerRow.append('<th>Answers</th>');
+				header.html(headerRow);
+				table.append(header);
+				var body = $('<tbody></tbody>');
+				table.append(body);
+				if (data != null && data.items != null) {
+					$.each(data.items, function (index, item) {
+						var row = $('<tr></tr>');
+
+						var colTitle = $('<td></td>');
+						var link = $('<a></a>');
+						link.addClass('ellipse');
+						link.html(item.title);
+						link.attr('href', item.link);
+						link.attr('target', '_blank');
+						colTitle.append(link);
+
+						var colAnswers = $('<td></td>');
+						colAnswers.append(item.answer_count);
+
+						row.append(colTitle);
+						row.append(colAnswers);
+
+						body.append(row);
+					});
+				}
+				$('#so_questions .card-block').html(table);
+			},
+			error: function () {
+				alert('Error');
+			}
+		});
+	}
+
+	function loadStackOverflowUsers() {
+		$.ajax({
+			url: "https://api.stackexchange.com/2.2/users?page=1&pagesize=5&order=desc&sort=reputation&site=stackoverflow",
+			type: "GET",
+			dataType: "json",
+			cache: false,
+			success: function (data) {
+				console.log(data);
+				var table = $('<table></table>');
+				table.addClass('table table-striped');
+				var header = $('<thead></thead>');
+				var headerRow = $('<tr></tr>');
+				headerRow.append('<th>User Name</th>');
+				headerRow.append('<th>Reputation</th>');
+				header.html(headerRow);
+				table.append(header);
+				var body = $('<tbody></tbody>');
+				table.append(body);
+				if (data != null && data.items != null) {
+					$.each(data.items, function (index, item) {
+						var row = $('<tr></tr>');
+
+						var colName = $('<td></td>');
+						var userlink = $('<a></a>');
+						userlink.addClass('ellipse');
+						userlink.html(item.display_name);
+						userlink.attr('href', item.link);
+						userlink.attr('target', '_blank');
+						colName.append(userlink);
+
+						var colReputation = $('<td></td>');
+						colReputation.append(item.reputation);
+
+						row.append(colName);
+						row.append(colReputation);
+
+						body.append(row);
+					});
+				}
+				$('#so_users .card-block').html(table);
+			},
+			error: function () {
+				alert('Error');
+			}
+		});
+	}
+});
