a2347aaad83b80609f2948151f0e518de3cdc465
daniel sarabia viera <dani_sv_ctv@hotmail.com>
2017-09-12 10:47:09 +0200

    Add logging to the contact page

--- MvcDemo/Controllers/ContactController.cs
+++ MvcDemo/Controllers/ContactController.cs
@@ -9,6 +9,7 @@ using MvcDemo.Configuration;
 using Microsoft.Extensions.Options;
 using Newtonsoft;
 using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Logging;
 
 namespace MvcDemo.Controllers
 {
@@ -17,14 +18,16 @@ namespace MvcDemo.Controllers
 		#region Members
 
 		private MyAppSettings _settings;
+		private ILogger<ContactController> _logger;
 
 		#endregion
 
 		#region Constructors
 
-		public ContactController(IOptions<MyAppSettings> settings)
+		public ContactController(IOptions<MyAppSettings> settings, ILogger<ContactController> logger)
 		{
 			_settings = settings.Value;
+			_logger = logger;
 		}
 
 		#endregion
@@ -36,37 +39,45 @@ namespace MvcDemo.Controllers
 
 		[HttpPost]
 		public IActionResult Index(ContactViewModel viewModel) {
-
-			if (this.ModelState.IsValid == false)
-				return View(viewModel);
-
-			// Validate Google reCaptcha
-			var captchaHeader = this.Request.Form["g-recaptcha-response"];
-			var client = new WebClient();
-			var result = client.DownloadString(string.Format(_settings.ReCaptchaUrl, _settings.ReCaptchaKey, captchaHeader));
-			var response = JObject.Parse(result);
-			var status = (bool)response.SelectToken("success");
-			if (status == false)
-				throw new Exception("ReCaptcha header is incorrect");
-
-			// Send email
-			var smtpClient = new System.Net.Mail.SmtpClient(_settings.SmtpServer, _settings.SmtpPort)
+			try
 			{
-				Credentials = new NetworkCredential(_settings.Email, _settings.EmailPassword),
-				EnableSsl = false
-			};
+				if (this.ModelState.IsValid == false)
+					return View(viewModel);
 
-			var body = "Full Name : " + viewModel.FullName + Environment.NewLine +
-					   "Company : " + viewModel.Company + Environment.NewLine +
-					   "Phone : " + viewModel.Phone + Environment.NewLine +
-					   "Email : " + viewModel.Email + Environment.NewLine +
-					   "Message : " + viewModel.Message + Environment.NewLine;
+				// Validate Google reCaptcha
+				var captchaHeader = this.Request.Form["g-recaptcha-response"];
+				var client = new WebClient();
+				var result = client.DownloadString(string.Format(_settings.ReCaptchaUrl, _settings.ReCaptchaKey, captchaHeader));
+				var response = JObject.Parse(result);
+				var status = (bool)response.SelectToken("success");
+				if (status == false)
+					throw new Exception("ReCaptcha header is incorrect");
 
-			smtpClient.Send(_settings.Email, _settings.Email, "Contact Form", body);
+				// Send email
+				var smtpClient = new System.Net.Mail.SmtpClient(_settings.SmtpServer, _settings.SmtpPort)
+				{
+					Credentials = new NetworkCredential(_settings.Email, _settings.EmailPassword),
+					EnableSsl = false
+				};
 
-			// Clear form
-			this.ModelState.Clear();
-			return View(new ContactViewModel { Success = true });
+				var body = "Full Name : " + viewModel.FullName + Environment.NewLine +
+						   "Company : " + viewModel.Company + Environment.NewLine +
+						   "Phone : " + viewModel.Phone + Environment.NewLine +
+						   "Email : " + viewModel.Email + Environment.NewLine +
+						   "Message : " + viewModel.Message + Environment.NewLine;
+
+				smtpClient.Send(_settings.Email, _settings.Email, "Contact Form", body);
+
+				// Clear form
+				this.ModelState.Clear();
+				return View(new ContactViewModel { Success = true });
+
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex.Message);
+				throw ex;
+			}
 		}
 	}
 }
